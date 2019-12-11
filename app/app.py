8b60a4dd0f74a3c70b77fe28df84d9649800d5ab
Elena-decathlon <elena.serebryakova@decathlon.com>
2019-12-10 18:03:25 -0800

    controling the password hashing as well as a different user sessions

--- app/app.py
+++ app/app.py
@@ -1,11 +1,18 @@
-from flask import Flask, flash, request, render_template, redirect, session, url_for
+from flask import Flask, flash, make_response, request, render_template, redirect, session, url_for
 from datetime import timedelta
 from app.forms import LoginForm
+from werkzeug.security import check_password_hash
+import logging
+
+logging.basicConfig(filename='logs/log_in.log',level=logging.DEBUG)
+#logging.debug('This message should go to the log file')
+#logging.info('So should this')
+#logging.warning('And this, too')
 
 flask_app = Flask(__name__)
 flask_app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
 
 from app.models import db, User
 from flask_login import LoginManager, login_user, login_required, logout_user
@@ -13,8 +20,10 @@ from flask_login import LoginManager, login_user, login_required, logout_user
 login_manager = LoginManager()
 login_manager.init_app(flask_app)
 
-import app.theft_e
-import app.theft_r
+import app.theft_exit_p
+import app.theft_exit_em
+import app.theft_rest_p
+import app.theft_rest_em
 
 @flask_app.before_request
 def before_request():
@@ -23,7 +32,10 @@ def before_request():
 
 @flask_app.after_request
 def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate,post-check=0, pre-check=0'"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    response.headers['Cache-Control'] = 'public, max-age=0'
     return(response)
 
 @login_manager.unauthorized_handler
@@ -43,13 +55,18 @@ def login():
     elif request.method == 'POST':
         if form.validate_on_submit():
             user = User.query.filter_by(email=form.email.data).first()
+            password = request.form['password']
+            email = request.form['email']
             if user:
-                if user.password == form.password.data:
+                if user and check_password_hash(user.password, password):
+                    session['email'] = email #saving the login for the session
+                    print("PRINTING HERE {}".format(session['email']))
                     login_user(user)
 #                    return "User logged in"
 #                    flash("You're now logged in!")
                     print("Login success")
-                    return(render_template('gates-selection.html'))
+#                    logging.info('%s logged in successfully', user.email)
+                    return(redirect(url_for('gates_selection')))
                 else:
 #                   return "Wrong password"
 #                    flash("No user with that email/password combo")
@@ -70,8 +87,8 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return(render_template("logout.html"))
-
+#    return(render_template("logout.html"))
+    return(redirect(url_for("login")))
 
 @login_manager.user_loader
 def load_user(email):
@@ -98,12 +115,23 @@ def gates_selection():
 @flask_app.route("/monitoring-exit")
 @login_required
 def monitoring():
-    pics = app.theft_e.get_pic()
-    return(render_template("monitoring.html", pics=pics))
-
+    if session['email'] == 'lena':
+        pics = app.theft_exit_p.get_pic()
+        gates_info = "Entrance/Exit"
+        return(render_template("monitoring.html", pics=pics, gates_info=gates_info))
+    elif session['email'] == 'test':
+        pics = app.theft_exit_em.get_pic()
+        gates_info = "Entrance/Exit"
+        return(render_template("monitoring.html", pics=pics, gates_info=gates_info))
 
 @flask_app.route("/monitoring-restroom")
 @login_required
 def monitoring_2():
-    pics = app.theft_r.get_pic()
-    return(render_template("monitoring.html", pics=pics))
+    if session['email'] == 'lena':
+        gates_info = "Restroom"
+        pics = app.theft_rest_p.get_pic()
+        return(render_template("monitoring.html", pics=pics, gates_info=gates_info))
+    elif session['email'] == 'test':
+        gates_info = "Restroom"
+        pics = app.theft_rest_em.get_pic()
+        return(render_template("monitoring.html", pics=pics, gates_info=gates_info))
---
+++ app/theft_exit_p.py
@@ -0,0 +1,60 @@
+from datetime import datetime, timedelta
+import json
+import requests
+import sys
+import time
+
+import  app.decoder
+
+url = "https://sf-dkt-gates-store-api-2214.herokuapp.com/alerts/theft"
+
+payload = "{\n    \"api_key\": \"u7dcdj52df45y7a0avcba8idu56g17d4\",\n    \"id_gate\": 1\n}"
+
+headers = {
+    'Content-Type': "application/json",
+    'Accept': "*/*",
+    'Cache-Control': "no-cache",
+    'Host': "sf-dkt-gates-store-api-2214.herokuapp.com",
+    'Accept-Encoding': "gzip, deflate",
+    'Content-Length': "72",
+    'Referer': "https://sf-dkt-gates-store-api-2214.herokuapp.com/alerts/theft",
+    'Connection': "keep-alive",
+    'cache-control': "no-cache"
+    }
+
+
+def get_pic():
+    '''
+    returning a list, containing a sorted data to display an entrance data
+    '''
+    response = requests.request("GET", url, data=payload, headers=headers)
+    obj = response.json()
+    gates = "entrance"
+    info_list = []
+    data = obj["data"]["results_data"]
+    ln = len(data)
+    for i in data:
+        dic = {}
+        if i["product_name"] == None:
+            name = "Picture is not found"
+            brand = ""
+            size = ""
+            pic = "../static/images/not_found.png"
+        else:
+            name = i["product_name"]
+            pic = i["product_picture"]
+            brand = i["product_brand"]
+            size = i["product_size"]
+        t_str = i["created"]
+        t_str_rep = t_str.replace(" GMT", "")                             #deleting the timezone name form the string
+        t_time = datetime.strptime(t_str_rep, '%a, %d %b %Y %H:%M:%S')
+        time = t_time - timedelta(hours=8, minutes = 0)                   #converting time to a PTZ    #converting str to datetime object
+        epc = i["product_epc"]
+        ean13 = app.decoder.EPC_decoder_EAN13(epc)
+        serial = app.decoder.EPC_decoder_serial(epc)
+        dic.update({"name": name, "pic": pic, "brand": brand, "size": size, "time": time, "epc": epc, "ean13": ean13, "serial": serial})
+        info_list.append(dic)
+#        print(info_list)
+    return(info_list)
+
+get_pic()
