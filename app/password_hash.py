ff1ca2a8ee14826a302fda35b5bcb28a77c041a1
Elena-decathlon <elena.serebryakova@decathlon.com>
2019-12-11 11:14:56 -0800

    removing hashing programm

--- app/password_hash.py
+++
@@ -1,8 +0,0 @@
-import sys
-from werkzeug.security import generate_password_hash
-
-def pass_hash(password):
-    hashed_pwd = generate_password_hash(password, 'sha256')
-    print("Hashed password is {}".format(hashed_pwd))
-
-pass_hash(sys.argv[1])
