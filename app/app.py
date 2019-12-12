9874fa6ee02722ecc2ffb7a27ddbaf58a0a388ab
Elena-decathlon <elena.serebryakova@decathlon.com>
2019-12-12 12:56:51 -0800

    commenting the log line

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from app.forms import LoginForm
 from werkzeug.security import check_password_hash
 import logging
 
-logging.basicConfig(filename='logs/log_in.log',level=logging.DEBUG)
+#logging.basicConfig(filename='logs/log_in.log',level=logging.DEBUG)
 #logging.debug('This message should go to the log file')
 #logging.info('So should this')
 #logging.warning('And this, too')
